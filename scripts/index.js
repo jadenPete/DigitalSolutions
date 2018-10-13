2cb621ec1d532d7d52ffa44b0dd05ceed2255da1
Jaden Peterson <jadenpeterson150@gmail.com>
2018-10-12 18:09:18 -0700

    Added new pages, changed styles, added comments section, etc

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
 new Typed(".products", {
 	loop: true,
 	startDelay: 1000,
-	strings: [ "Websites", "Apps", "Servers", "Cloud", "AI" ],
+	
+	strings: [
+		"Websites",
+		"Apps",
+		"Servers",
+		"Cloud"
+	],
 	
 	backSpeed: 50,
 	typeSpeed: 100,
